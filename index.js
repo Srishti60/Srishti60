422448422820c8164a6c319b96bb5617b2417d90
Srishti <srishti.11.pal@gmail.com>
2024-04-19 15:21:19 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form');
 const fruits = document.querySelector('.fruits');
-
 // edit button------>
+
 const newBtn = document.querySelectorAll('li');
 newBtn.forEach((item) =>{
   const editBtn = document.createElement('button');
@@ -13,7 +13,6 @@ newBtn.forEach((item) =>{
 form.addEventListener('submit', function (event){
     event.preventDefault();
     // for Addition-------->
-
     const fruitToAdd = document.getElementById('fruit-to-add');
     const newLi = document.createElement('li');
     const newLiText = document.createTextNode(fruitToAdd.value);
@@ -25,7 +24,11 @@ form.addEventListener('submit', function (event){
     btn.appendChild(btnText);
     newLi.appendChild(btn);
     fruits.appendChild(newLi);
-   const edBtn = document.createElement('button');
+
+   // Clear the input field
+   fruitToAdd.value = ''; // Setting the input field value to an empty string
+
+    const edBtn = document.createElement('button');
    const edBtnText = document.createTextNode('Edit');
    edBtn.appendChild(edBtnText);
    edBtn.className = 'edit-btn';
@@ -33,14 +36,26 @@ form.addEventListener('submit', function (event){
 })
 
 // delete item------->
+
 fruits.addEventListener('click',function(event){
    if(event.target.classList.contains('delete-btn'))
    {
     const fruitsToDelete = event.target.parentElement;
     fruits.removeChild(fruitsToDelete);
    }
- 
 })
+
+//  edit items------->
+fruits.addEventListener('click', function(event) {
+   if (event.target.classList.contains('edit-btn')) {
+     const editFruit = event.target.parentElement;
+     const fruitText = editFruit.firstChild.textContent; // Get the text content of the item to edit
+     document.getElementById("fruit-to-add").value = fruitText; // Set the value of the input field to the text content of the item to edit
+ 
+     // Remove the item to edit from the list
+     fruits.removeChild(editFruit);
+   }
+ });
 
 
 // filter items-------->
